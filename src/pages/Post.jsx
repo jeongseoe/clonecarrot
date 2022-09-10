a34b881b655e4bc636deddcc36c59e5c66d73255
daehyunkim <find.acho@gmail.com>
2022-09-10 20:11:53 +0900

    대현 브랜치 첫 커밋 테스트

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Post = () => {
     return (
-        <div>게시물 작성 페이지 입니다.</div>
+        <div>게시물 작성 페이지 입니다.
+            이곳은 대현의 작업 구간입니다.
+        </div>
     );
 };
 
