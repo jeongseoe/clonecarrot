b498612af44977da8973224e8d31ffc20de6bb9c
jeongseoe <gldi1633@naver.com>
2022-09-14 20:51:09 +0900

    헤더 완료!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import logo from "../../logo.svg";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { __logout, logout } from "../../redux/modules/userSlice";
 
 const Header = () => {
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const onClickHandler = () => {
-        // dispatch(__logout());
-        // dispatch(logout());
+        dispatch(__logout());
+        dispatch(logout());
         window.alert("로그인 페이지로 이동합니다");
         navigate('/');
     };
+
+    const token = localStorage.getItem('RefreshToken')
 
 
 
@@ -22,20 +26,35 @@ const Header = () => {
     return (
         <>
             <StContainer>
-                <StImg src={logo} alt=" carrot_1" />
+                <StImg src={logo} alt=" carrot_1" onClick={() => navigate('/list')} />
                 <StBtnContainer>
                     {/* 로그인누르면 로그인화면, 로그아웃누르면 쿠키제거하기 */}
-                    { }
-
-                    <StButton >
+                    {token === null ? <StButton onClick={() => navigate('/')}>
                         로그인
-                    </StButton>
+                    </StButton> : <StButton onClick={onClickHandler}>
+                        로그아웃
+                    </StButton>}
+
+                    {token === null ? null :
+
+                        <StButton onClick={() => navigate('/mypage')}>
+                            마이페이지
+                        </StButton>
+                    }
+                    {token === null ? null :
+
+                        <StButton onClick={() => navigate('/post')}>
+                            내물건 팔기
+                        </StButton>
+                    }
+
+
+
                     <StButton onClick={() => navigate('/signup')}>
                         회원가입
                     </StButton>
-                    <StButton onClick={() => navigate('/mypage')}>
-                        마이페이지
-                    </StButton>
+
+
                 </StBtnContainer>
             </StContainer>
         </>
